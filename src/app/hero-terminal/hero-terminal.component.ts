91913b872cb6d138d03005b58b662d0fed38059d
aryanVijaywargia <aryanvijaywargia@gmail.com>
2023-07-22 04:21:24 +0530

    adding portfolio hero page

---
+++ src/app/hero-terminal/hero-terminal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-terminal',
+  templateUrl: './hero-terminal.component.html',
+  styleUrls: ['./hero-terminal.component.scss']
+})
+export class HeroTerminalComponent {
+
+}
