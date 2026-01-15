69c5e926b7697d4bb80a42b4562654be74767f14
aryanVijaywargia <aryanvijaywargia@gmail.com>
2026-01-15 19:47:58 +0530

    update app module, routing, and main component
    Remove unused imports and simplify app structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component, HostListener, Renderer2 } from '@angular/core';
-import { ThemeService } from './theme.service';
+import { ThemeService } from './shared/services/theme.service';
 import { Subscription } from 'rxjs';
 import * as AOS from 'aos';
 import { Router, NavigationEnd } from '@angular/router';
-import { ScrollSpyService } from './scroll-spy.service';
+import { ScrollSpyService } from './shared/services/scroll-spy.service';
 
 @Component({
   selector: 'app-root',
@@ -12,29 +12,24 @@ import { ScrollSpyService } from './scroll-spy.service';
 })
 export class AppComponent {
 
-  // constructor (){};
 
-  ngOnInit(){
+  ngOnInit() {
     AOS.init()
-    // this.router.events.subscribe((event) => {
-    document.body.classList.remove('nb-theme-dark'); 
-  // });
+    document.body.classList.remove('nb-theme-dark');
   }
 
-//   ngOnInit(): void {
-    
-// }
+
 
   receivedTheme: any;
   private subscription: Subscription;
 
-  constructor(private themeService: ThemeService, private router: Router,private renderer: Renderer2, private scrollSpyService: ScrollSpyService) {
+  constructor(private themeService: ThemeService, private router: Router, private renderer: Renderer2, private scrollSpyService: ScrollSpyService) {
     this.subscription = this.themeService.sharedData$.subscribe(data => {
       this.receivedTheme = data;
     });
   }
 
-  
+
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,55 +5,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-// import {MatIconModule} from '@angular/material/icon';
-// import {MatTabsModule} from '@angular/material/tabs';
-import { TerminalComponent } from './terminal/terminal.component';
-import { SearchComponent } from './search/search.component';
 import { CardTitleComponent } from './card-title/card-title.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { FaceComponent } from './face/face.component';
-import { OptionButtonComponent } from './option-button/option-button.component';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { CardComponent } from './card/card.component';
-import { PhoneComponent } from './phone/phone.component';
-// import { NavbarComponent } from './navbar/navbar.component';
-import { TabsComponent } from './tabs/tabs.component';
 import { ClarityModule } from '@clr/angular';
-import { TransitionDirective } from './Directives/transition.directive';
+import { TransitionDirective } from './shared/directives/transition.directive';
 import { ProjectComponent } from './project/project.component';
-import { ScrollGalleryComponent } from './scroll-gallery/scroll-gallery.component';
-import { ImageComponent } from './image/image.component';
+import { ScrollGalleryComponent } from './shared/components/scroll-gallery/scroll-gallery.component';
+import { ImageComponent } from './shared/components/image/image.component';
 import { AboutComponent } from './about/about.component';
-import { HeroComponent } from './hero/hero.component';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { RangeSliderComponent } from './range-slider/range-slider.component';
 import { HeroTerminalComponent } from './hero-terminal/hero-terminal.component';
-// import { CodeComponent } from './code/code.component';
 import { TimelineComponent } from './time-line/time-line.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { DemoTerminalComponent } from './demo-terminal/demo-terminal.component';
 import { TerminalNewComponent } from './terminal-new/terminal-new.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DemoCardComponent } from './demo-card/demo-card.component';
 import { DarkmodeIconComponent } from './darkmode-icon/darkmode-icon.component';
-import { ImageSliderComponent } from './image-slider/image-slider.component';
-// import { PrismModule } from '@ngx-prism/core';
-// import { PrismModule } from '@ngx-prism/core';
-
-// import { NgbModule, NgbNav, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FooterComponent } from './footer/footer.component';
-import { DarkModeNavComponent } from './dark-mode-nav/dark-mode-nav.component';
-import { FormButtonComponent } from './form-button/form-button.component';
 import { NbChatModule, NbIconModule, NbLayoutModule, NbSidebarModule, NbThemeModule } from '@nebular/theme';
 import { ChatbotUiComponent } from './chatbot-ui/chatbot-ui.component';
-import { ChatbotSidenavComponent } from './chatbot-sidenav/chatbot-sidenav.component';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { BadgeComponent } from './badge/badge.component';
 import { RouterModule, Routes } from '@angular/router';
-import { ScrollPositionDirective } from './scroll-position.directive';
+import { ScrollPositionDirective } from './shared/directives/scroll-position.directive';
 import { BurgerMenuComponent } from './burger-menu/burger-menu.component';
 import { HamburgerIconComponent } from './hamburger-icon/hamburger-icon.component';
 import { CardSubmitButtonComponent } from './card-submit-button/card-submit-button.component';
@@ -63,54 +42,38 @@ library.add(fab);
 
 const routes: Routes = [
   { path: '', component: HeroSectionComponent },
-  // Add more routes as needed
 ];
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TerminalComponent,
-    // AutofocusDirective,
-    SearchComponent,
-    PhoneComponent,
     NavbarComponent,
-    AboutMeComponent,
-    OptionButtonComponent,
-    CardComponent,
     FaceComponent,
     CardTitleComponent,
     ExperienceComponent,
-    TabsComponent,
     TransitionDirective,
     ProjectComponent,
     ScrollGalleryComponent,
     ImageComponent,
     AboutComponent,
-    HeroComponent,
     HeroSectionComponent,
     RangeSliderComponent,
     HeroTerminalComponent,
     TimelineComponent,
-    DemoTerminalComponent,
     TerminalNewComponent,
     DemoCardComponent,
     DarkmodeIconComponent,
-    ImageSliderComponent,
     FooterComponent,
-    DarkModeNavComponent,
-    FormButtonComponent,
     ChatbotUiComponent,
-    ChatbotSidenavComponent,
     BadgeComponent,
     ScrollPositionDirective,
     BurgerMenuComponent,
     HamburgerIconComponent,
     CardSubmitButtonComponent
-    // CodeComponent
   ],
-  
-  
+
+
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -131,9 +94,8 @@ const routes: Routes = [
       useHash: true,
       anchorScrolling: 'enabled'
     })
-    // MatSidenavModule
   ],
-  exports:[CommonModule],
+  exports: [CommonModule],
   providers: [],
   bootstrap: [AppComponent]
 })
