212b7517cc9b0c2d893c43cced5da0a44821f8df
aryanVijaywargia <aryanvijaywargia@gmail.com>
2026-01-15 19:46:26 +0530

    remove old services and directives relocated to shared module

--- src/app/scroll-position.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ScrollPositionDirective } from './scroll-position.directive';
-
-describe('ScrollPositionDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ScrollPositionDirective();
-    expect(directive).toBeTruthy();
-  });
-});
