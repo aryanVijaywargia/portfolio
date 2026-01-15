11b0cace85ab965208ae5d671e99e8a796d43b9f
aryanVijaywargia <aryanvijaywargia@gmail.com>
2026-01-15 19:47:40 +0530

    update about and face components
    Refactor about section and face component styling

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ABOUT } from './ABOUT';
-import { HeroHeightService } from '../hero-height.service';
+import { HeroHeightService } from '../shared/services/hero-height.service';
 import { TimelineComponent } from '../time-line/time-line.component';
 import { Subscription } from 'rxjs';
 
@@ -10,18 +10,14 @@ import { Subscription } from 'rxjs';
 })
 export class AboutComponent {
   @ViewChild('buttonRef') buttonRef!: ElementRef<HTMLButtonElement>;
-  focusImageIndex:number = 0;
-  image:any= ABOUT.images; 
-  tooltip:any = ABOUT.tooltip;
-  index:number = 0;
-  rangeIndex:string="";
+  focusImageIndex: number = 0;
+  image: any = ABOUT.images;
+  tooltip: any = ABOUT.tooltip;
+  index: number = 0;
+  rangeIndex: string = "";
   ABOUT = ABOUT;
 
-  // trackRangeValue(newValue: string) {
-  //   this.rangeIndex = newValue;
-  // }
-
-  private subscription!:Subscription;
+  private subscription!: Subscription;
 
   @ViewChild(TimelineComponent) timelineComponent!: TimelineComponent;
 
@@ -32,16 +28,12 @@ export class AboutComponent {
   constructor(private heroHeightService: HeroHeightService) {
   }
 
-  ngOnInit(){
-    // this.timelineComponent.startHighlightingSequence();
+  ngOnInit() {
   }
-
-  // rangeIndex!: number;
 
   onSelectedValueChange(value: any) {
     this.rangeIndex = value;
     console.log("The vlaue of this.rangeIndex is ", this.rangeIndex)
-    // Do something with the selected value in the parent component
   }
 
 
@@ -53,7 +45,7 @@ export class AboutComponent {
     this.timelineComponent.lastBarHighlighted.subscribe(() => {
       setTimeout(() => {
         this.timelineComponent.startHighlightingSequence();;
-      }, 2000); // Add a delay before restarting the sequence
+      }, 2000);
     });
   }
 
